9ad90edbae6d6edc4382f1f12dc58b2b5dc7133c
KPHAM <Kennypham12323@gmail.com>
2022-06-16 13:50:16 -0700

    Finished Etch-A-Sketch

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-
-function grid(){
-    let num = 16;
+var num = 16;
+function grid(squares){
+    let width = 560;
     let grid = document.querySelector('div')
-    for (let c=0; c<num; c++){
+    /* make rows */
+    for (let c=0; c<squares; c++){
         let column = document.createElement('div');
         column.classList.add('column');
-        for (let r=0; r<num; r++){
+        let c_width = width/squares;
+        column.style.cssText = "display: flex; justify-content: space-evenly; border: 0;"
+        column.style.width = width + 'px';
+        column.style.height = c_width + 'px';
+        /* make squares in each row */
+        for (let r=0; r<squares; r++){
             let row = document.createElement('div');
+            let r_width= width/squares;
+            row.style.height = r_width + 'px';
+            row.style.width = r_width + 'px';
             row.classList.add('row');
-            column.appendChild(row);
+            column.appendChild(row);   
         }
+        /* add rows to container */
         grid.appendChild(column);
     }
-
 }
-grid();
+grid(num);
+
+function inp1(){
+   let new1 = prompt('Enter a number 2-100');
+   console.log(new1);
+   const old_row = document.querySelectorAll('.row');
+   old_row.forEach(element => element.remove());
+   const old_column = document.querySelectorAll('.column');
+   old_column.forEach(element => element.remove());
+   grid(new1);
+   let sketch = document.querySelectorAll('.row');
+   sketch.forEach((row) => {
+      row.addEventListener('mouseover', event => {row.style.background = 'black'})
+   });
+}
+
+function white(){
+  const old_row = document.querySelectorAll('.row');
+  old_row.forEach(element => element.style.background= 'white');
+}
+
+var sketcher = document.querySelectorAll('.row');
+sketcher.forEach((row) => {
+    row.addEventListener('mouseover', event => {row.style.background = 'black'})
+});
+
+
